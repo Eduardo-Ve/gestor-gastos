9679a949115dad31a5fcf6078c4cb79981f635d5
Eduardo Velasquez <ed.velasquez@duocuc.cl>
2026-08-04 20:07:05 -0400

    primera subida del proyecto que tengo en mente

---
+++ lib/mock-data.ts
@@ -0,0 +1,13 @@
+// lib/mock-data.ts
+export const mockTransactions = [
+  { id: "1", type: "expense", amount: 15000, category: "Alimentación", date: "2026-08-01", description: "Supermercado", paymentMethod: "Débito" },
+  // ya obviamente esto es una transacción 
+];
+
+export const mockCategories = [
+  { id: "1", name: "Alimentación", type: "expense", color: "#f97316" },
+  // y esto es una categoria
+];
+
+
+//primeros datos de prueba que voy a mandar para ir probando
